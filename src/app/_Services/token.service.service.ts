30842b56b7a34dc40b12045c388b4968ac5d67e5
Christopher Havrez <c.havrez@stag.technofuturtic.education>
2024-01-27 21:20:38 +0100

    *** Gard login *** { - Vérification des route -Récuperation Token -Déconnection }

--- src/app/_Services/token.service.service.ts
+++ src/app/_Services/token.service.service.ts
@@ -1,19 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class TokenServiceService {
 
-  constructor() { }
+  constructor(
+    private router: Router
 
-  saveToken(token :string) :void {
-    localStorage.setItem('token',token)
+  ) { }
+
+  saveToken(token: string): void {
+    localStorage.setItem('token', token)
+  }
+  clearToken(): void {
+    localStorage.removeItem('token')
+    this.router.navigate(['/'])
   }
 
-  islogin():boolean{
-    const token =localStorage.getItem('token')
-    return !! token
+  islogin(): boolean {
+    const token = localStorage.getItem('token')
+    return !!token
   }
 
 }
--- src/app/admin/admin-layout/nav-barre-playout/nav-barre-playout.component.ts
+++ src/app/admin/admin-layout/nav-barre-playout/nav-barre-playout.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TokenServiceService } from 'src/app/_Services/token.service.service';
 
 @Component({
   selector: 'app-nav-barre-playout',
   templateUrl: './nav-barre-playout.component.html',
   styleUrls: ['./nav-barre-playout.component.css']
 })
-export class NavBarrePlayoutComponent {
+export class NavBarrePlayoutComponent implements OnInit {
 
+  constructor(
+    private tokenService: TokenServiceService
+
+  ) { }
+
+  ngOnInit(): void {
+
+  }
+
+  logout(): void {
+    this.tokenService.clearToken()
+  }
 }
