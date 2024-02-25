284a2e5621790488dc74e25982603e5f61c0b876
Havrez Christopher <havrez.christopher@outlook.com>
2024-02-25 20:09:41 +0100

    Article CRUD

--- src/app/_Services/article.service.service.ts
+++ src/app/_Services/article.service.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArticleServiceService {
 
-  constructor() { }
+  private API_URL=environment.API_URL
+
+  constructor(private http=HttpClient) { }
+
+    getAllArticle() : Observable<any>{
+      return this.http.get<any>(`${this.API_URL}/article`)
+
+    }
+    getArticle(idArticle: any): Observable<any> {
+      return this.http.get<any>(`${this.API_URL}/article/${idArticle}`)
+    }
+    addArticle(ArticleData: any): Observable<any> {
+      return this.http.put<any>(`${this.API_URL}/article`, ArticleData);
+    }
+    updateArticle(idArticle: any, ArticleData: any): Observable<any> {
+      return this.http.patch<any>(`${this.API_URL}/article/${idArticle}`, ArticleData);
+    }
+    trashArticle(){
+
+    }
+    untrashArticle(){
+
+    }
+    deleteArticle(){
+
+    }
+
+
+
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiErrorService } from './_Subject/api-error.service';
+import { KonamiCodeService } from './_Secret/konami-code.service';
 
 @Component({
   selector: 'app-root',
@@ -9,17 +10,26 @@ import { ApiErrorService } from './_Subject/api-error.service';
 export class AppComponent implements OnInit{
   title = 'Angular-Public-Admin';
   message = ''
+  display = false
 
-constructor(private apiErrorServices:ApiErrorService){}
+constructor(
+  private apiErrorServices:ApiErrorService,
+  private konamiCodeService: KonamiCodeService
+  
+  ){}
 
 
   ngOnInit(): void {
-    this.apiErrorServices.apiError.subscribe(data => {
+    this.konamiCodeService.listenKonamiCode();
+    this.apiErrorServices.apiError.subscribe(
+      data => {
       this.message = data;
+      this.display = true
       
     })
   }
   clearMessage (){
     this.message=''
+    this.display = false
   }
 }
