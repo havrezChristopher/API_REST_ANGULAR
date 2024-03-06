2df86a6ff9aa1d845fc2fbaee42b637d7219a0f5
Havrez Christopher <havrez.christopher@outlook.com>
2024-03-06 01:53:50 +0100

    GSAP (Animation!)

--- src/app/public/contact/contact.component.ts
+++ src/app/public/contact/contact.component.ts
@@ -18,15 +18,30 @@ export class ContactComponent implements AfterViewInit {
 
   ngAfterViewInit(): void {
     // Utilisez gsap avec scrollTrigger pour animer votre élément
+    // gsap.from(this.contactForm.nativeElement, {
+    //   scrollTrigger: {
+    //     trigger: this.contactForm.nativeElement,
+    //     start: 'top bottom', // L'animation commence lorsque le 'top' du trigger atteint le 'bottom' du viewport
+    //     toggleActions: 'play none none none' // Actions à effectuer à l'entrée du viewport, pendant le défilement et à la sortie
+    //   },
+    //   x: 600, // déplace l'élément de 100 pixels à partir de la droite
+    //   opacity: 0,
+    //   duration: 2.5 // durée de l'animation en secondes
+    // });
     gsap.from(this.contactForm.nativeElement, {
       scrollTrigger: {
         trigger: this.contactForm.nativeElement,
-        start: 'top bottom', // L'animation commence lorsque le 'top' du trigger atteint le 'bottom' du viewport
-        toggleActions: 'play none none none' // Actions à effectuer à l'entrée du viewport, pendant le défilement et à la sortie
+        start: 'top center ',
+        end: 'center center',
+        toggleActions: 'play none none reverse',
+        markers: true // Utile pour le débogage, à enlever en production
       },
-      x: 600, // déplace l'élément de 100 pixels à partir de la droite
-      opacity: 0,
-      duration: 2.5 // durée de l'animation en secondes
+      x: 600, // Commence à 100 pixels à droite de sa position d'origine
+      opacity: 0, // Commence invisible
+      duration: 1, // Durée de l'animation en secondes
+      ease: 'power2.out', // Type d'assouplissement (easing)
+      delay: 1.2, // Retard avant le début de l'animation
     });
+ 
   }
 }
