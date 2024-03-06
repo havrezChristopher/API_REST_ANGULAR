d6e32b8350e24a2bb4e40b171864c209f8b2292c
Havrez Christopher <havrez.christopher@outlook.com>
2024-03-06 01:36:59 +0100

    init GSAP pour la pages contact

--- src/app/public/contact/contact.component.ts
+++ src/app/public/contact/contact.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component ,ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+// Enregistrez le plugin ScrollTrigger
+gsap.registerPlugin(ScrollTrigger);
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent {
+export class ContactComponent implements AfterViewInit {
+  @ViewChild('contactForm') contactForm: ElementRef<HTMLDivElement>={} as ElementRef<HTMLDivElement>;
 
+  constructor() { 
+    // Enregistrez le plugin ScrollTrigger
+    gsap.registerPlugin(ScrollTrigger);
+  }
+
+  ngAfterViewInit(): void {
+    // Utilisez gsap avec scrollTrigger pour animer votre élément
+    gsap.from(this.contactForm.nativeElement, {
+      scrollTrigger: {
+        trigger: this.contactForm.nativeElement,
+        start: 'top bottom', // L'animation commence lorsque le 'top' du trigger atteint le 'bottom' du viewport
+        toggleActions: 'play none none none' // Actions à effectuer à l'entrée du viewport, pendant le défilement et à la sortie
+      },
+      x: 600, // déplace l'élément de 100 pixels à partir de la droite
+      opacity: 0,
+      duration: 2.5 // durée de l'animation en secondes
+    });
+  }
 }
