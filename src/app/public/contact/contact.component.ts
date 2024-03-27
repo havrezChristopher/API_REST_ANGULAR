8af7c65401b68f19f0e56f8f13846db52a1887a2
Havrez Christopher <havrez.christopher@outlook.com>
2024-03-27 17:17:16 +0100

    Update

--- src/app/public/contact/contact.component.ts
+++ src/app/public/contact/contact.component.ts
@@ -1,19 +1,19 @@
-import { Component ,ElementRef, ViewChild, AfterViewInit } from '@angular/core';
-import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+// import { gsap } from 'gsap';
+// import { ScrollTrigger } from 'gsap/ScrollTrigger';
 // Enregistrez le plugin ScrollTrigger
-gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollTrigger);
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements AfterViewInit {
-  @ViewChild('contactForm') contactForm: ElementRef<HTMLDivElement>={} as ElementRef<HTMLDivElement>;
+  @ViewChild('contactForm') contactForm: ElementRef<HTMLDivElement> = {} as ElementRef<HTMLDivElement>;
 
-  constructor() { 
+  constructor() {
     // Enregistrez le plugin ScrollTrigger
-    gsap.registerPlugin(ScrollTrigger);
+    // gsap.registerPlugin(ScrollTrigger);
   }
 
   ngAfterViewInit(): void {
@@ -27,21 +27,21 @@ export class ContactComponent implements AfterViewInit {
     //   x: 600, // déplace l'élément de 100 pixels à partir de la droite
     //   opacity: 0,
     //   duration: 2.5 // durée de l'animation en secondes
+    // // });
+    // gsap.from(this.contactForm.nativeElement, {
+    //   scrollTrigger: {
+    //     trigger: this.contactForm.nativeElement,
+    //     start: 'top center  ',
+    //     end: 'center center',
+    //     toggleActions: 'play none none reverse',
+    //     // markers: true // Utile pour le débogage, à enlever en production
+    //   },
+    //   x: 600, // Commence à 100 pixels à droite de sa position d'origine
+    //   opacity: 0, // Commence invisible
+    //   duration: 1, // Durée de l'animation en secondes
+    //   ease: 'power2.out', // Type d'assouplissement (easing)
+    //   delay: 0.6, // Retard avant le début de l'animation
     // });
-    gsap.from(this.contactForm.nativeElement, {
-      scrollTrigger: {
-        trigger: this.contactForm.nativeElement,
-        start: 'top center  ',
-        end: 'center center',
-        toggleActions: 'play none none reverse',
-        // markers: true // Utile pour le débogage, à enlever en production
-      },
-      x: 600, // Commence à 100 pixels à droite de sa position d'origine
-      opacity: 0, // Commence invisible
-      duration: 1, // Durée de l'animation en secondes
-      ease: 'power2.out', // Type d'assouplissement (easing)
-      delay: 0.6, // Retard avant le début de l'animation
-    });
- 
+
   }
 }
